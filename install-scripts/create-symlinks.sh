166c408cda34c57fed2170261503fc169f5609a9
Zachary Durgan <ztdurgan@umich.edu>
2020-09-11 20:51:23 -0400

    Symlinks script: gitconfig, bashrc, and emacs set up

--- install-scripts/create-symlinks.sh
+++ install-scripts/create-symlinks.sh
@@ -1,3 +1,9 @@
+sudo rm -rf ~/.gitconfig
+sudo rm -rf ~/.bashrc
+sudo rm -rf ~/.emacs.d
+
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfile/.emacs ~/.emacs
+
+mkdir ~/.emacs.d
+ln -s ~/dotfiles/.emacs ~/.emacs.d/init.el
